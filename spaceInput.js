afd6c0d99a2030ea1cae65daaa8573343bf08c90
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2022-10-12 13:32:15 -0400

    Update spaceInput.js

--- spaceInput.js
+++ spaceInput.js
@@ -1,13 +1,18 @@
+import { spaceAnswers } from "./spaceAnswers.js";
 import { answer, next } from "./spaceSetup.js";
 
 export const check = document.getElementById('inputField').innerText;
 export const enterButton = document.getElementById('checkButton').click();
 export let inputBox = document.getElementById('inputField');
+export let realAnswer = spaceAnswers[0].answer;
 
 inputBox.onkeyup = function() {
     console.log(inputBox.value);
     checkAnswer();
+    checkLetters();
 };
+
+
 
 function checkAnswer(){
     if (inputBox.value.toLowerCase()  == answer.toLowerCase()) {
@@ -30,3 +35,4 @@ function clear(){
         inputBox.value = "";
     }
 }
+
