58161784ddbcddbc82368ee25a917a2243b092d1
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-01-11 16:58:20 -0500

    Initial Commit, never uploaded to github

--- spaceSetup.js
+++ spaceSetup.js
@@ -1,15 +1,66 @@
-let c = document.getElementById('spaceCanvas-ship');
-let ctx = c.getContext("2d");
-ctx.arc(160, 65, 40, 0, 2 * Math.PI);
-ctx.strokeStyle = 'blue';
-ctx.stroke();
+import { spaceAnswers } from "./spaceAnswers.js";
 
-let a = document.getElementById('spaceCanvas-blasterA');
-let abc = a.getContext("2d");
-abc.beginPath();
-abc.rect(160, 100, 15, 10);
-abc.strokeStyle = 'blue'
-abc.stroke();
+const nextButton = document.getElementById('rightButton').addEventListener ('click', next);
+const previousButton = document.getElementById('leftButton').addEventListener('click', previous);
+const displayText = document.getElementById('spaceText');
+const displayDefinition = document.getElementById('spaceGameDefinition');
 
-// this was great work today, great work on finishing out the first game's innards and moving onto the next. 
-// also good job with stepping out of your comfort zone with using the canvas element
+let currentText = 0;
+let currentDefinition = 0;
+let currentId = spaceAnswers[0].answerInput;
+let currentIdB = spaceAnswers[0].definition;
+
+
+
+
+console.log(currentId)
+for (let i = 0; i < spaceAnswers.length; i++) {
+    displayText.innerHTML = JSON.stringify(spaceAnswers[0].answerInput);
+}
+
+for (let i = 0; i < spaceAnswers.length; i++) {
+    displayDefinition.innerHTML = JSON.stringify(spaceAnswers[0].definition);
+}
+
+log();
+
+function next()
+{
+    move();
+}
+
+function previous()
+{   
+    move(false);
+}
+
+
+
+
+function move(advance = true)
+{
+
+    currentText = (currentText + (advance ? 1 : -1) + spaceAnswers.length) % spaceAnswers.length;
+    currentId = spaceAnswers[currentText].answerInput;
+
+    currentDefinition = (currentDefinition + (advance ? 1 : -1) + spaceAnswers.length) % spaceAnswers.length;
+    currentIdB = spaceAnswers[currentText].definition;
+
+    displayText.innerHTML = JSON.stringify(currentId);
+    displayDefinition.innerHTML = JSON.stringify(currentIdB);
+
+    
+
+    // previousText = (currentText + (advance ? 1: -1) + spaceAnswers.length) % spaceAnswers.length;
+    // previousId = spaceAnswers[previousText].answer;
+
+    // previousDefinition = (currentDefinition + (advance ? 1 : -1) + spaceAnswers.length) % spaceAnswers.length;
+    // previousIdB = spaceAnswers[previousIdB].definition;
+    log();
+}
+
+function log()
+{
+    console.log("Word: " + currentText,
+    currentId);
+}
